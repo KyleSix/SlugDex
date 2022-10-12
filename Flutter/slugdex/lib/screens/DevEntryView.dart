736f1e765c72c9b3eb0fb92489a28ad0b7758c91
Bayley Barreuther <bay8142@gmail.com>
2022-10-11 18:25:46 -0700

    Individual Entry Page, UI Update, Undiscovered, Prep for Objects
    Added: Pages for all entries when given in form of a list, updated the UI, added support for undiscovered locations, prepped for the addition of objects.

--- Flutter/slugdex/lib/screens/DevEntryView.dart
+++ Flutter/slugdex/lib/screens/DevEntryView.dart
@@ -1,20 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:slugdex/screens/DexEntryPage.dart';
 
 class dexEntryView extends StatelessWidget {
+  const dexEntryView({required this.entry});
+  final dexEntry entry;
   @override
   Widget build(BuildContext context) {
+    String date = entry.discoveredDate;
     return Scaffold(
-      appBar: AppBar(title: const Text('SlugDex'),),
-      body: Center(
-        child: Column(
-          children: const <Widget> [
-            Text(
-              "Entry", 
-              textAlign: TextAlign.center, 
-              style: TextStyle(fontSize: 50),),
-          ],
-          ),
+      appBar: AppBar(
+        title: const Text('SlugDex'),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
       ),
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(24),
+                  bottomRight: Radius.circular(24),
+                ),
+                color: Colors.yellow.shade700,
+              ),
+              alignment: Alignment.topCenter,
+              height: MediaQuery.of(context).size.height /2.5,
+              child: Column(
+                children: [
+                  Text(entry.name, style: const TextStyle(fontSize: 50, fontWeight: FontWeight.w500, color: Colors.white),),
+                  Container(
+                    width: 225.0,
+                    height: 225.0,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white, width: 3.0),
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: NetworkImage(entry.image),
+                        )
+                    ),
+                  ),
+                ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text("Rarity:", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500, color: Colors.black),),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text("Description:", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500, color: Colors.black),),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(entry.description, style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w500, color: Colors.black),),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text("Discovered: " + date, style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500, color: Colors.black),),
+            ),
+          ),
+      ],
+    ),
     );
   }
 }
--- Flutter/slugdex/lib/screens/DexEntryPage.dart
+++ Flutter/slugdex/lib/screens/DexEntryPage.dart
@@ -1,7 +1,44 @@
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'DevEntryView.dart';
 
+// TEMP CODE ===========================================================================================
+class dexEntry {
+  final String name;
+  final String description;
+  final String image;
+  final double latitude;
+  final double longitude;
+  final bool discovered;
+  final String discoveredDate;
+
+  const dexEntry(this.name, this.description, this.image,this.latitude, this.longitude, this.discovered, this.discoveredDate);
+}
+
+isDiscovered(number) {
+  if(number % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// TEMP CODE ===========================================================================================
+
 class dexEntryPage extends StatelessWidget {
+  // TEMP CODE ===========================================================================================
+  final dexEntries = List.generate(
+  30,
+  (i) => dexEntry(
+    'Entry $i', 
+    'Decription for entry $i',
+    'https://i.imgur.com/YFoAUPD.jpg', 
+    20.2342342, 
+    -32.3222332,
+    isDiscovered(i),
+    "10/11/2022"
+  )
+  );
+  // TEMP CODE ===========================================================================================
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +57,20 @@ class dexEntryPage extends StatelessWidget {
             crossAxisSpacing: 10,
             mainAxisSpacing: 10,
             ),
-            // Sub 10 for: dexEntrys.length or such
-            itemCount: 10,
+            itemCount: dexEntries.length,
             itemBuilder: (BuildContext context, int index) {
+              Color col;
+              String img;
+              String entryname;
+              if(dexEntries[index].discovered){
+                col = Colors.yellow.shade700;
+                img = dexEntries[index].image;
+                entryname = dexEntries[index].name;
+              } else {
+                col = Colors.grey;
+                img = "https://i.imgur.com/9eaDFaP.png";
+                entryname = "Undiscovered";
+              }
               return Padding(
                 padding: const EdgeInsets.all(2.0),
                 child: InkWell(
@@ -30,7 +78,7 @@ class dexEntryPage extends StatelessWidget {
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
                       border: Border.all(color: Colors.blue.shade800, width: 3.0),
-                      color: Colors.yellow.shade700,
+                      color: col,
                     ),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
@@ -42,22 +90,23 @@ class dexEntryPage extends StatelessWidget {
                           decoration: BoxDecoration(
                               border: Border.all(color: Colors.white, width: 3.0),
                               shape: BoxShape.circle,
-                              image: const DecorationImage(
+                              image: DecorationImage(
                               fit: BoxFit.fill,
-                              image: NetworkImage(
-                                    "https://i.imgur.com/YFoAUPD.jpg")
-                                    )
+                              image: NetworkImage(img),
+                              )
                           )
                         ),
                         //Image.asset(subjects[index].subjectImage, fit: BoxFit.cover, height: 50, width: 50,),
                         const SizedBox(height: 15,),
                         //Sub "Entry name for: dexEntrys[index].entryName or similar"
-                        Text("Entry $index", style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: Colors.white),),
+                        Text(entryname, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: Colors.white),),
                       ],
                     ),
                   ),
                   onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => dexEntryView()));
+                    if(dexEntries[index].discovered) {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => dexEntryView(entry: dexEntries[index])));
+                    } 
                   },
                 ),
               );
