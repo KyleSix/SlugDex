0ecbe77c0e7718b912e8bd1ca856133d68c202a9
Bayley Barreuther <bay8142@gmail.com>
2022-10-24 17:44:56 -0700

    Fixed

--- Flutter/slugdex/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter/slugdex/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import geolocator_apple
 import location
 import package_info_plus
+import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
