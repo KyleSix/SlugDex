d418991bc78fff1f92ce8f7b5ddc576266be3406
Troys-Code <troyandrewschultz@gmail.com>
2022-11-07 12:48:28 -0800

    Logo improved

--- Flutter/slugdex/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter/slugdex/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import geolocator_apple
 import location
 import package_info_plus
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
