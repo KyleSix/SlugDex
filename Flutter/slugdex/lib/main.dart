f19524f5e7745d1c3d11bb538b56b0a136a4e1e4
Jonathan Aguirre <jomaguir@ucsc.edu>
2022-11-07 12:46:59 -0800

    added firebase

--- Flutter/slugdex/lib/main.dart
+++ Flutter/slugdex/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:slugdex/screens/LiveMapScreen.dart';
@@ -5,13 +6,19 @@ import 'package:slugdex/Entry/entryReadWrite.dart';
 import 'Entry/entry.dart';
 import 'package:slugdex/provider/LocationProvider.dart';
 import 'dart:core';
+//Firebase Imports
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';//Firebabse Authentication
+
 
 List<Entry> entryList = []; //Global ist of all entries
+var db = FirebaseFirestore.instance;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   entryList = await loadEntry();
-
   runApp(MyApp());
 }
 
