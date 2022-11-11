6bf08dc0e90c5f7bac963c8598dfb2ed04ab3ccd
Kyle L <kyslim@ucsc.edu>
2022-11-11 04:47:53 -0800

    Linked Settings and Account Auth. New Profile UI

--- Flutter/slugdex/lib/screens/settingsPage.dart
+++ Flutter/slugdex/lib/screens/settingsPage.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
+import 'package:slugdex/settings/settingsTools.dart';
+import 'package:slugdex/screens/editProfilePage.dart';
 
 const double spacing = 16.0;
+const double icon_size = 24.0;
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -10,7 +13,15 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  bool logoutEnabled = true; // Do some check to see if we're logged in
+  // Get User info
+  final User? user = FirebaseAuth.instance.currentUser;
+
+  // Configure settings
+  bool logoutEnabled = (FirebaseAuth.instance.currentUser != null)
+      ? true
+      : false; // Check to see if we're logged in
+
+  // Store settings
   static const keyDevMode = "key-dev-mode";
 
   @override
@@ -24,7 +35,7 @@ class _SettingsPageState extends State<SettingsPage> {
         ),
         backgroundColor: Color.fromARGB(255, 230, 230, 230),
         body: ListView(
-          padding: EdgeInsets.all(24),
+          padding: EdgeInsets.all(8),
           children: [
             ////// The User profile Icon and Name ///////
             WrapperWidget(
@@ -48,19 +59,23 @@ class _SettingsPageState extends State<SettingsPage> {
                   ],
                 ),
               ],
-              background_color: Color.fromARGB(255, 255, 208, 0),
+              background_color: slugdex_yellow,
             ),
             ////// The First Group of Settings ////
             SettingsGroup(
-              title: "GENERAL",
+              title: "ACCOUNT",
+              titleTextStyle:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
               children: <Widget>[
                 const SizedBox(height: 4),
                 WrapperWidget(children: [
-                  const SizedBox(height: 10),
+                  const SizedBox(height: 2),
+                  buildEditProfile(), // Edit Profile Button
+                  const SizedBox(height: spacing),
                   buildLogout(), // Logout Button
                   const SizedBox(height: spacing),
                   buildDeleteAccount(), // Delete Account Button
-                  const SizedBox(height: 10),
+                  const SizedBox(height: 2),
                 ]),
               ],
             ),
@@ -70,12 +85,18 @@ class _SettingsPageState extends State<SettingsPage> {
             ////// The Second Group of Settings ////
             SettingsGroup(
               title: "MISC",
+              titleTextStyle:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
               children: <Widget>[
                 const SizedBox(height: 4),
                 WrapperWidget(children: [
-                  const SizedBox(height: 10),
-                  buildDevMode(),
-                  const SizedBox(height: 10),
+                  const SizedBox(height: 2),
+                  buildBugReport(), // Bug Report Button
+                  const SizedBox(height: spacing),
+                  buildFeedback(), // Feedback Button
+                  const SizedBox(height: spacing),
+                  buildDevMode(), // Dev Mode Button
+                  const SizedBox(height: 2),
                 ]),
               ],
             ),
@@ -83,98 +104,111 @@ class _SettingsPageState extends State<SettingsPage> {
         ),
       );
 
+  /// Edit Profile Settings ///
+  Widget buildEditProfile() => SimpleSettingsTile(
+        title: "Edit Profile",
+        subtitle: "Appearance, Username",
+        leading: IconWidget(
+            icon: Icons.edit_note, color: Colors.greenAccent, size: icon_size),
+        enabled: logoutEnabled,
+        child: EditProfilePage(),
+      );
+
+  /// Logout Setting ///
   Widget buildLogout() => SimpleSettingsTile(
       title: "Logout",
       subtitle: "",
-      leading:
-          IconWidget(icon: Icons.logout, color: Colors.blueAccent, size: 30.0),
+      leading: IconWidget(
+          icon: Icons.logout, color: Colors.blueAccent, size: icon_size),
       enabled: logoutEnabled,
-      onTap: () {
-        if (!logoutEnabled) return;
+      onTap: () async {
+        if (!logoutEnabled) return; // Only works if we're signed in
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Logging Out...")),
+          SnackBar(
+              content: Text("Logging Out..."),
+              duration: const Duration(milliseconds: 2000)),
         );
-        logoutEnabled = false;
+        logoutEnabled = false; // Set our logout flag to disable setting
+        await FirebaseAuth.instance.signOut(); // do sign out
+        Navigator.pop(context); // Go back to sign-in screen
       });
+
+  /// Delete Account Setting ///
   Widget buildDeleteAccount() => SimpleSettingsTile(
       title: "Delete Account",
       subtitle: "",
       leading: IconWidget(
-          icon: Icons.delete_forever, color: Colors.redAccent, size: 30.0),
-      onTap: () {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Deleting Account...")),
-        );
-      });
-  
+          icon: Icons.delete_forever, color: Colors.grey, size: icon_size),
+      enabled: logoutEnabled,
+
+      /// On Tap displays Alert Dialog for deletion confirmation ///
+      onTap: () => showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: const Text('Account Deletion Warning'),
+              content:
+                  const Text('Are you sure you would to delete your account?'),
+              actions: <Widget>[
+                /// Account deletion canceled ///
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'Cancel'),
+                  child: const Text('Cancel'),
+                ),
+
+                /// Account deletion confirmed ///
+                TextButton(
+                  onPressed: () async {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                          content: Text("Deleting Account..."),
+                          duration: const Duration(milliseconds: 2000)),
+                    );
+                    logoutEnabled = false;
+                    await user?.delete(); // do account deletion
+                    Navigator.pop(
+                        context); // Go back to sign-in screen, 2 pops needed
+                    Navigator.pop(context);
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            ),
+          ));
+
+  Widget buildBugReport() => SimpleSettingsTile(
+        title: "Report A Bug",
+        leading: IconWidget(
+            icon: Icons.bug_report, color: Colors.grey, size: icon_size),
+        onTap: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+                content: Text("Feature in progress..."),
+                duration: const Duration(milliseconds: 2000)),
+          );
+        },
+      );
+
+  Widget buildFeedback() => SimpleSettingsTile(
+        title: "Send Feedback",
+        leading:
+            IconWidget(icon: Icons.email, color: Colors.grey, size: icon_size),
+        onTap: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+                content: Text("Feature in progress..."),
+                duration: const Duration(milliseconds: 2000)),
+          );
+        },
+      );
+
   Widget buildDevMode() => SwitchSettingsTile(
         title: "Developer Mode",
+        subtitle: "",
         settingKey: keyDevMode,
-        leading: IconWidget(icon: Icons.developer_mode, color: Colors.grey),
+        leading: IconWidget(
+            icon: Icons.developer_mode, color: Colors.grey, size: icon_size),
         onChange: (value) {
           debugPrint(keyDevMode + ": $value");
         },
-      );
-}
-
-// This class wraps an Icon in a container of a specific shape and color
-// Usage: IconWidget(icon: Icons.menu, color: Colors.black)
-class IconWidget extends StatelessWidget {
-  final IconData icon;
-  final Color color;
-  final double size;
-  final double radius;
-  final Color icon_color;
-
-  const IconWidget(
-      {Key? key,
-      required this.icon,
-      required this.color,
-      this.size = 24.0,
-      this.radius = 16.0,
-      this.icon_color = Colors.white})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => Container(
-        padding: EdgeInsets.all(6),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: color,
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(radius)),
-          color: color,
-        ),
-        child: Icon(
-          icon,
-          color: icon_color,
-          size: size,
-        ),
-      );
-}
-
-// This class wraps any collections of setting tile in a white container with rounded edges
-class WrapperWidget extends StatelessWidget {
-  final List<Widget> children;
-  final Color background_color;
-  const WrapperWidget({
-    Key? key,
-    required this.children,
-    this.background_color = Colors.white,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => Container(
-        padding: EdgeInsets.all(6),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: background_color,
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(16.0)),
-          color: background_color,
-        ),
-        child: Column(
-          children: children,
-        ),
       );
 }
